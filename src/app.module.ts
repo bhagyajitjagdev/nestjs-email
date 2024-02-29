86bbc6d4bd21e0eec8868846ecef2d6c21751924
Bhagyajit Jagdev <bhagyajit.sbj@gmail.com>
2024-02-29 12:13:38 +0530

    import TypedEventEmitterModule in the app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { EmailModule } from './modules/email/email.module';
 import { UserModule } from './modules/user/user.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { TypedEventEmitterModule } from './event-emitter/typed-event-emitter.module';
 
 @Module({
-  imports: [EmailModule, UserModule, EventEmitterModule.forRoot()],
+  imports: [
+    EmailModule,
+    UserModule,
+    EventEmitterModule.forRoot(),
+    TypedEventEmitterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
